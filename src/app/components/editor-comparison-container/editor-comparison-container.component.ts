4d596ce14e02206494527da2929bdbb106f611eb
parathkumar <parathk.tup@gmail.com>
2025-01-07 15:43:22 +0530

    display only quill with sanitize

--- src/app/components/editor-comparison-container/editor-comparison-container.component.ts
+++ src/app/components/editor-comparison-container/editor-comparison-container.component.ts
@@ -4,6 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { FroalaEditorModule } from 'angular-froala-wysiwyg';
 import { QuillEditorComponent, QuillModule, QuillModules } from 'ngx-quill';
 import Table from 'quill/modules/table';
+import * as sanitize from 'sanitize-html';
+import { quillModuleConfig } from './editor-comparison-container.helper';
 
 
 @Component({
@@ -14,45 +16,22 @@ import Table from 'quill/modules/table';
   styleUrl: './editor-comparison-container.component.scss'
 })
 export class EditorComparisonContainerComponent {
-  froalaContent = '';
+  //froalaContent = '';
   quillContent = '';
-  quillComponent = viewChild<QuillEditorComponent>('quillComp'); 
-  froalaOptions = {
-    pluginsEnabled: ['table', 'align', 'link', 'image'],
-    toolbarButtons: ['insertTable', 'bold', 'italic', 'underline', 'formatOL', 'formatUL']
-  };
-  modules: QuillModules = {
-    toolbar: [
-      ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
-      ['blockquote', 'code-block'],
-  
-      [{ 'header': 1 }, { 'header': 2 }],               // custom button values
-      [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-      [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
-      [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
-      [{ 'direction': 'rtl' }],                         // text direction
-  
-      [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
-      [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-  
-      [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
-      [{ 'font': [] }],
-      [{ 'align': [] }],
-  
-      ['clean'],                                         // remove formatting button
-  
-      ['link'],
-      ['table']
-    ],
-    table: true
-  };
+  sanitizedContent = '';
+  quillComponent = viewChild<QuillEditorComponent>('quillComp');
+  // froalaOptions = {
+  //   pluginsEnabled: ['table', 'align', 'link', 'image'],
+  //   toolbarButtons: ['insertTable', 'bold', 'italic', 'underline', 'formatOL', 'formatUL']
+  // };
+  modules = quillModuleConfig;
   tableModule = computed(() => this.quillComponent()?.quillEditor.getModule('table') as Table);
   protected openDocumentation(url: string) {
     window.open(url, '_blank');
   }
   protected logContent() {
-    console.log(this.froalaContent);
+    //console.log(this.froalaContent);
     console.log(this.quillContent);
-    console.log(this.tableModule().getTable());
+    this.sanitizedContent = sanitize(this.quillContent);
   }
 }
